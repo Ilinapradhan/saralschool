e3c7693886ef547e1e5da43e0799b0e31ac8fa37
ilinapradhan <ilina04khushi@gmail.com>
2024-02-18 01:17:25 +0530

    login / registration

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,10 +1,17 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
+import Image from "next/image";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
-        <SideNav />
+      <Image
+            src="/saralschool.png"
+            width={400}
+            height={400}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,50 +5,54 @@ import { lusitana } from "@/app/ui/fonts";
 import Link from "next/link";
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
+    <main className="flex min-h-screen flex-col p-6 ">
+      <div className="flex justify-between h-15 shrink-0 items-end rounded-lg  p-3 md:h-25 ">
+        {/* <AcmeLogo /> */}
+        <Image
+            src="/saralschool.png"
+            width={400}
+            height={400}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"></div>
+        <div className="flex flex-col justify-center md:h-25 gap-6 rounded-lg bg-white-50 px-6 py-10 md:w-2/5 md:px-15">
           <p
-            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
-          >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            className={`${lusitana.className} text-xl text-gray-800 md:text-4xl md:leading-normal`}
+          > 
+             <strong>Welcome to SaralSchool. </strong>  
+              Advance your engineering skills with us.{" "}
+            {/*<a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
-          </p>
+            ,brought to you by .  */}
+           </p>
+           <div className="flex grow flex-col gap-4 md:flex-row">
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span>
+            <span> Log in </span>
           </Link>
           <Link
             href="/register"
-            className="flex items-center gap-5 self-start rounded-lg bg-gray-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-gray-400 md:text-base"
+            className="flex items-center bg-black text-white gap-5 self-start rounded-lg px-6 py-3  text-sm font-medium transition-colors hover:bg-gray md:text-base "
           >
+            
             <span>Register</span>
           </Link>
+          </div>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div className="flex justify-center md:w-3/5 md:px-28">
           <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
+            src="/bg1.png"
+            width={400}
+            height={400}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop and mobile versions"
-          />{" "}
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className="block md:hidden"
-            alt="Screenshot of the dashboard project showing mobile version"
           />
+          
         </div>
       </div>
     </main>
